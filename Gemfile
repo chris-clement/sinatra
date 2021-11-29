bb4897cd6d45a7beca4f7f7dd15c1a1e6973883f
Francesco <francescocollotto@gmail.com>
2021-11-29 16:18:50 +0000

    add some routes

--- Gemfile
+++ Gemfile
@@ -9,3 +9,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem "sinatra", "~> 2.1"
 
 gem "webrick", "~> 1.7"
+
+gem "sinatra-contrib", "~> 2.1"
--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
-  "Hello World"
+  "Hello!"
+end
+
+get '/secret' do
+  "You found the secret route!"
+end
+
+get '/makers' do
+  "This is now updated"
+end
+
+get '/coffee' do
+  "It's time for coffee"
 end
