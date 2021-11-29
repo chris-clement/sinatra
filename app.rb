4b16e8ef6f124e959247614c8c2d800a8146fe87
Chris Clement <chrisclement@Chriss-Air.connect>
2021-11-29 16:56:46 +0000

    Add cat route to show img

--- app.rb
+++ app.rb
@@ -16,3 +16,14 @@ end
 get '/coffee' do
   "It's time for coffee"
 end
+
+get '/cat' do
+  "<div style='border: 10px solid blue'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+   </div>"
+end
+
+get '/time' do
+  code = "<%= Time.now %>"
+  erb code
+end
